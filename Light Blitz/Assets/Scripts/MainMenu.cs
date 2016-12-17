f902b7dda2be1d413c8bb7c00686dab22d5a83c6
jpegfreak17 <jpegfreak17@gmail.com>
2016-12-18 02:41:13 +0800

    Light Blitz
    Pause and Main menu added

---
+++ Light Blitz/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    public string startLevel;
+
+    public void Play()
+    {
+        Application.LoadLevel(startLevel);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+	
+}
